d935b6289c80bbe788f806c9e2fd88aaf4a0d408
Aliyan Siddiqui <141032178+Aliyan555-555@users.noreply.github.com>
2024-12-05 18:11:14 +0500

    updatye

--- src/components/client/socialMedia.tsx
+++ src/components/client/socialMedia.tsx
@@ -5,8 +5,8 @@ const Page = () => {
 <iframe
   src="https://plugins.flockler.com/embed/iframe/193963555b50d1809da8a2a58b3931c8/1939639dca50f6f3ac03a0a384db0fed"
   id="flockler-embed-iframe-1939639dca50f6f3ac03a0a384db0fed" 
-  style={{display:'block', border: 'none', width:'100%'}}
-  className='h-fit'
+  style={{display:'block', border: 'none', width:'100%',height:'150vh'}}
+
   allowFullScreen>
 </iframe>
   )
